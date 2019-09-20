5bf03a4cce85b6bf6beff52810a7d4c37b086cea
Михаил <dr.mihail@yandex.ru>
2019-09-20 22:29:18 +0500

    добавлен deploy в gh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Конфигуратор окон
+
+[view demo](https://mihail-174.github.io/react-configurator-window-aksioma174/)
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './style/App.css';
 const Context = React.createContext()
 
 let initialState = {
-    winds: 1,
+    winds: 2,
     width: 1300,
     height: 1400,
     height_wind: 1400,
